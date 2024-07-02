f43db678a59386b1aec6598a24ce496fb8f7a6ee
Hyeonmin Choi <hymn9613@gmail.com>
2024-07-02 17:47:10 +0900

    chore: 프로젝트 디렉토리 구조 설정

---
+++ src/app/(afterLogin)/explore/page.tsx
@@ -0,0 +1,3 @@
+export default function Explore() {
+  return <div>EXPLORE PAGE</div>;
+}
---
+++ src/app/(beforeLogin)/layout.tsx
@@ -0,0 +1,8 @@
+export default function BeforeLoginLayout({ children }) {
+  return (
+    <div>
+      BEFORE LOGIN LAYOUT
+      {children}
+    </div>
+  );
+}
